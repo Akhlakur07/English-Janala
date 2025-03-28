001811af9ce3159cc9292a3a9b2e5b19c4608ce5
Akhlakur Rahman <ssrmm2000@gmail.com>
2025-03-28 17:44:39 +0600

    lesson buttons added

--- script/index.js
+++ script/index.js
@@ -1,1 +1,24 @@
-console.log('Hello, world!');
+function loadLessons() {
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+        .then(res => res.json())
+        .then(data => displayLessons(data.data))
+}
+
+function displayLessons(lessons) {
+    const lessonContainer = document.getElementById('lesson-container');
+
+    for (let i of lessons) {
+        const lessonDiv = document.createElement('div');
+
+        lessonDiv.innerHTML = `
+        <button class="btn btn-outline btn-primary">
+            <i class="fa-solid fa-book-open"></i>
+            Lesson -${i.level_no}
+        </button>`;
+
+        lessonContainer.appendChild(lessonDiv);
+    }
+}
+
+
+loadLessons()
