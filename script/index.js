979456e50c7ed027f16b8192cdabacd40d259b3e
Akhlakur Rahman <ssrmm2000@gmail.com>
2025-03-29 18:06:04 +0600

    login logout done

--- script/index.js
+++ script/index.js
@@ -1,47 +1,47 @@
 function loadLessons() {
-    fetch("https://openapi.programming-hero.com/api/levels/all")
-        .then(res => res.json())
-        .then(data => displayLessons(data.data))
+  fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res) => res.json())
+    .then((data) => displayLessons(data.data));
 }
 
 function displayLessons(lessons) {
-    const lessonContainer = document.getElementById('lesson-container');
+  const lessonContainer = document.getElementById("lesson-container");
 
-    for (let i of lessons) {
-        const lessonDiv = document.createElement('div');
+  for (let i of lessons) {
+    const lessonDiv = document.createElement("div");
 
-        lessonDiv.innerHTML = `
+    lessonDiv.innerHTML = `
         <button id="btn-${i.level_no}" onclick="loadWords(${i.level_no})" class="btn btn-outline btn-primary">
             <i class="fa-solid fa-book-open"></i>
             Lesson -${i.level_no}
         </button>`;
 
-        lessonContainer.appendChild(lessonDiv);
-    }
+    lessonContainer.appendChild(lessonDiv);
+  }
 }
 
-let activeCheck = '';
+let activeCheck = "";
 
 function loadWords(id) {
-    url = `https://openapi.programming-hero.com/api/level/${id}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            const active = document.getElementById(`btn-${id}`);
-            if (activeCheck!=='') {
-                activeCheck.classList.add('btn-outline');
-            }
-            active.classList.remove('btn-outline');
-            activeCheck = active;
-            displayWords(data.data)
-        })
+  url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      const active = document.getElementById(`btn-${id}`);
+      if (activeCheck !== "") {
+        activeCheck.classList.add("btn-outline");
+      }
+      active.classList.remove("btn-outline");
+      activeCheck = active;
+      displayWords(data.data);
+    });
 }
 
 function displayWords(words) {
-    const wordContainer = document.getElementById('word-container');
-    wordContainer.innerHTML = "";
-    if (words.length == 0) {
-        wordContainer.innerHTML = `
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
+  if (words.length == 0) {
+    wordContainer.innerHTML = `
         <div class="grid place-items-center col-span-3 py-16 gap-5">
             <img class="mx-auto" src="./assets/alert-error.png" alt="">
             <p class="bangla text-lg font-normal text-center opacity-80">
@@ -54,11 +54,11 @@ function displayWords(words) {
 
           </div>
         `;
-        return;
-    }
-    for (let i of words) {
-        const wordDiv = document.createElement('div');
-        wordDiv.innerHTML = `
+    return;
+  }
+  for (let i of words) {
+    const wordDiv = document.createElement("div");
+    wordDiv.innerHTML = `
           <div class="card bg-base-100 h-[420px] shadow-md p-14">
             <p class="text-center text-[32px] font-bold mb-6">${i.word}</p>
             <p class="text-center text-xl font-medium mb-6">Meaning /Pronounciation</p>
@@ -68,44 +68,48 @@ function displayWords(words) {
                     <div id="word-${i.id}" onclick="loadDetails(${i.id})" class="bg-zinc-100 size-14 rounded-xl flex justify-center items-center">
                         <i class="fa-solid fa-circle-exclamation"></i>
                     </div>
-                    <div class="bg-zinc-100 size-14 rounded-xl flex justify-center items-center">
+                    <div onclick="loadWordSpeak(${i.id})" class="bg-zinc-100 size-14 rounded-xl flex justify-center items-center">
                         <i class="fa-solid fa-volume-high"></i>
                     </div>
                 </div>
             </div>
         </div>
-
-
         
         `;
 
-        wordContainer.appendChild(wordDiv);
-    }
+    wordContainer.appendChild(wordDiv);
+  }
 }
 
 function loadDetails(id) {
-    url = `https://openapi.programming-hero.com/api/word/${id}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayDetails(data.data))
+  console.log(id)
+  url = `https://openapi.programming-hero.com/api/word/${id}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayDetails(data.data));
 }
 
 function displayDetails(data) {
-    const popupContainer = document.getElementById('popup-container');
-    // popupContainer.innerHTML = "";
-    synonyms = data.synonyms;
-    console.log(synonyms);
-    const details = document.createElement('div');
-    details.innerHTML = `
+  const popupContainer = document.getElementById("popup-container");
+  synonyms = data.synonyms;
+  console.log(synonyms);
+  const details = document.createElement("div");
+  details.innerHTML = `
         <div
           class="fixed inset-0 bg-black/20 flex justify-center items-center"
         >
           <div class="card bg-base-100 shadow-xl p-12 w-[735px]">
-            <p class="text-4xl font-semibold mb-8">${data.word} (<span><i class="fa-solid fa-microphone"></i></span>:${data.pronunciation})</p>
+            <p class="text-4xl font-semibold mb-8">${
+              data.word
+            } (<span><i class="fa-solid fa-microphone"></i></span>:${
+    data.pronunciation
+  })</p>
             <p class="text-xl font-semibold mb-[10px]">
               Meaning
             </p>
-            <p class="bangla text-2xl font-medium opacity-80 mb-8">${data.meaning}</p>
+            <p class="bangla text-2xl font-medium opacity-80 mb-8">${
+              data.meaning
+            }</p>
 
             <p class="text-2xl font-semibold mb-2">
                 Example
@@ -118,21 +122,60 @@ function displayDetails(data) {
                 সমার্থক শব্দ গুলো
             </p>
             <div class="flex gap-3">
-                ${synonyms.map(syn => `<div class="bg-zinc-100 p-3 rounded-md">${syn}</div>`).join("")}
+                ${synonyms
+                  .map(
+                    (syn) =>
+                      `<div class="bg-zinc-100 p-3 rounded-md">${syn}</div>`
+                  )
+                  .join("")}
             </div>
             
             <button onclick="closepop()" class="btn btn-primary rounded-lg w-[269px] mt-12">Complete Learning</button>
           </div>
         </div>
     `;
-    popupContainer.appendChild(details);
+  popupContainer.appendChild(details);
 }
 
 function closepop() {
-    const popup = document.getElementById("popup-container");
-    popup.innerHTML = "";
+  const popup = document.getElementById("popup-container");
+  popup.innerHTML = "";
 }
 
+function btnLogin() {
+  const name = document.getElementById("name");
+  const password = document.getElementById("password");
+  const hero = document.getElementById("hero")
+  const navbar = document.getElementById("navbar")
+  const learnSection = document.getElementById("learn-section")
+  const faqSection = document.getElementById("faq-section")
+  if (name.value === "" || password.value === "") {
+    alert("Please fill in all fields");
+  }
+  if (password.value != "123456") {
+    alert("Incorrect Password")
+  }
+  else {
+    hero.classList.add("hidden")
+    navbar.classList.remove("hidden")
+    learnSection.classList.remove("hidden")
+    faqSection.classList.remove("hidden")
+  }
+}
 
+function loadWordSpeak(id){
+    console.log(id)
+    url = `https://openapi.programming-hero.com/api/word/${id}`;
+    fetch(url)
+      .then(res => res.json())
+      .then(data => pronounceWord(data.data.word));
+}
 
-loadLessons()
+function pronounceWord(word) {
+    console.log(word)
+    const utterance = new SpeechSynthesisUtterance(word);
+    utterance.lang = 'en-EN';
+    window.speechSynthesis.speak(utterance);
+  }
+
+loadLessons();
